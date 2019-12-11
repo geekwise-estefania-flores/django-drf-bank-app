12fe7c5d93c49af7670ad6fd8aa900afd225e263
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-12-11 23:17:49 +0000

    install: installed django-heroku 0.3.1

--- app/main/settings.py
+++ app/main/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# Heroku 
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -32,6 +34,7 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
 'PAGE_SIZE' : 10
 }
+
 
 
 INSTALLED_APPS = [
@@ -123,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
