048db363df3726b717bb159c6ad503b694f6f5c5
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-12-12 20:01:24 +0000

    update:setup serializers for models

--- app/bank/serializers.py
+++ app/bank/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers 
+
 class User_Serializers( serializers.HyperlinkedModelSerializer ):
 	class Meta:
 		model = User 
@@ -9,3 +10,23 @@ class Group_Serializers( serializers.HyperlinkedModelSerializer ):
 	class Meta:
 		model = Group 
 		fields = ['url', 'name']
+
+class Account_Serializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['url', 'account_number']
+
+# class Branch_Serializers(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Branch
+#         fields = []
+
+class Customer_Serializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['customer_name', 'customer_email']
+
+class Product_Serializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['product_options', 'card_options']
