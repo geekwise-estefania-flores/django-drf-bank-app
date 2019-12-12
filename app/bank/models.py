c08f11091ff0322956726d9ce6b264e1e523bb36
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-12-12 18:14:03 +0000

    update: added models for account, branch, customer, product

--- app/bank/models.py
+++ app/bank/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+#Account model
+class Account(models.Model):
+    account_number = models.CharField(max_length = 4)
+
+#branch model
+class Branch(models.Model):
+  
+  class Meta:
+    verbose_name_plural = "branches"
+
+  bank = models.ForeignKey('auth.User', related_name='bank', null=True,
+    on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.bank
+
+
+#customer model
+class Customer(models.Model):
+  customer_name = models.CharField(max_length = 30)
+  customer_email = models.EmailField(max_length = 100)
+
+  account = models.ForeignKey('auth.User', related_name='account', null=True,
+    on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.account
+
+#product model
+class Product(models.Model):
+  
+  product_options = (
+    ('savings', 'SAVINGS'),
+    ('checking', 'CHECKING'),
+    )
+  card_options = (
+    ('debit', 'DEBIT'),
+    ('credit', 'CREDIT')
+  )
+  product_options = models.CharField(max_length = 8, choices = product_options, default = product_options[0])
+
+  card_options = models.CharField(max_length = 8,
+  choices = card_options, default = card_options[0])
+
+  product = models.OneToOneField(
+    Branch, 
+    on_delete=models.CASCADE, 
+    primary_key=True)
+
+  def __str__(self):
+    return self.bank
